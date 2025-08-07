6bae2ce3d597e085df321e6d2cd5f37098e164bf
Vasyl Boliukh <vskesha@gmail.com>
2025-08-08 02:26:20 +0300

    added model fixes

--- taxi/models.py
+++ taxi/models.py
@@ -1,28 +1,37 @@
+from django.conf import settings
 from django.contrib.auth.models import AbstractUser
 from django.db import models
 
 
 class Manufacturer(models.Model):
-    name = models.CharField(max_length=255, unique=True)
-    country = models.CharField(max_length=255)
+    name = models.CharField(max_length=63, unique=True)
+    country = models.CharField(max_length=63)
 
     class Meta:
         ordering = ("name", )
 
+    def __str__(self) -> str:
+        return self.name
 
 class Driver(AbstractUser):
-    license_number = models.CharField(max_length=255, unique=True)
+    license_number = models.CharField(max_length=63, unique=True)
 
     class Meta:
         ordering = ("username", )
 
+    def __str__(self) -> str:
+        return f"{self.username}: ({self.first_name} {self.last_name})"
+
 
 class Car(models.Model):
-    model = models.CharField(max_length=255)
+    model = models.CharField(max_length=63)
     manufacturer = models.ForeignKey(
         Manufacturer, on_delete=models.CASCADE, related_name="cars"
     )
-    drivers = models.ManyToManyField(Driver, related_name="cars")
+    drivers = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name="cars")
 
     class Meta:
         ordering = ("model", )
+
+    def __str__(self) -> str:
+        return f"{self.model}"
